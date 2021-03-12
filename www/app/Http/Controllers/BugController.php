e7ce4507534bb591f8ee02b46e9d856f247451ee
Mathilde <dutrieuxmathilde@gmail.com>
2021-03-12 10:11:03 +0100

    Bug page data

--- www/app/Http/Controllers/BugController.php
+++ www/app/Http/Controllers/BugController.php
@@ -60,10 +60,25 @@ class BugController extends Controller
         echo "cURL Error #:" . $err;
       } else {
         $response = json_decode($response);
+        $months = [
+          '1'=>'january',
+          '2'=>'february',
+          '3'=>'march',
+          '4'=>'april',
+          '5'=>'may',
+          '6'=>'june',
+          '7'=>'july',
+          '8'=>'august',
+          '9'=>'september',
+          '10'=>'october',
+          '11'=>'november',
+          '12'=>'december'
+        ];
       }
 
       return view('bug', [
-        'response' => $response
+        'response' => $response,
+        'months' => $months
       ]);
     }
 }
--- www/resources/views/bug.blade.php
+++ www/resources/views/bug.blade.php
@@ -3,4 +3,21 @@
   <img src="{{ $response->image_uri }}" height="300px" alt="{{ $response->name->{'name-EUen'} }} image">
   <p>Price : {{ $response->price }} bells</p>
   <p>Rarity : {{ $response->availability->rarity }}</p>
+  <p>Catch-phrase : {{ $response->{'catch-phrase'} }}</p>
+  <p>Availability time : {{ $response->availability->time }}</p>
+  <p>Availability months : 
+  @foreach ($response->availability->{'month-array-northern'} as $month_northern)
+    @if($loop->first)
+    {{ ucfirst($months[$month_northern]) }}
+    @endif
+
+    @if(!$loop->first)
+    {{ $months[$month_northern] }}
+    @endif
+    
+    @if(!$loop->last)
+    -
+    @endif
+  @endforeach
+  </p>
 </div>
