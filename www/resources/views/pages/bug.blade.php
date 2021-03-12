e941808b5303a326ca2ab1764154d3391c6e4eaa
Mathilde <dutrieuxmathilde@gmail.com>
2021-03-12 15:47:32 +0100

    Bug basic css

--- www/resources/views/pages/bug.blade.php
+++ www/resources/views/pages/bug.blade.php
@@ -2,27 +2,31 @@
 @section('content')
 <section class="bug">
   <h1>{{ ucfirst($response->name->{'name-EUen'}) }}</h1>
-  <div>
-    <img src="{{ $response->image_uri }}" height="300px" alt="{{ $response->name->{'name-EUen'} }} image">
-    <p>Price : {{ $response->price }} bells</p>
-    <p>Rarity : {{ $response->availability->rarity }}</p>
-    <p>Catch-phrase : {{ $response->{'catch-phrase'} }}</p>
-    <p>Availability time : {{ $response->availability->time }}</p>
-    <p>Availability months : 
-    @foreach ($response->availability->{'month-array-northern'} as $month_northern)
-      @if($loop->first)
-      {{ ucfirst($months[$month_northern]) }}
-      @endif
+  <div class="content">
+    <div class="picture">
+      <img src="{{ $response->image_uri }}" alt="{{ $response->name->{'name-EUen'} }} image">
+    </div>
+    <div class="data">
+      <p>Price : {{ $response->price }} bells</p>
+      <p>Rarity : {{ $response->availability->rarity }}</p>
+      <p>Catch-phrase : {{ $response->{'catch-phrase'} }}</p>
+      <p>Availability time : {{ $response->availability->time }}</p>
+      <p>Availability months : 
+      @foreach ($response->availability->{'month-array-northern'} as $month_northern)
+        @if($loop->first)
+        {{ ucfirst($months[$month_northern]) }}
+        @endif
 
-      @if(!$loop->first)
-      {{ $months[$month_northern] }}
-      @endif
-      
-      @if(!$loop->last)
-      -
-      @endif
-    @endforeach
-    </p>
+        @if(!$loop->first)
+        {{ $months[$month_northern] }}
+        @endif
+        
+        @if(!$loop->last)
+        -
+        @endif
+      @endforeach
+      </p>
+    </div>
   </div>
 </section>
 @endsection
--- www/resources/views/pages/bugs.blade.php
+++ www/resources/views/pages/bugs.blade.php
@@ -6,7 +6,7 @@
     @foreach ($response as $i=>$row)
       <div>
         <h2>{{ ucfirst($row->name->{'name-EUen'}) }}</h2>
-        <a href="{{ route('bug',['id'=>$i]) }}"><img src="{{ $row->icon_uri }}" height="100" alt="{{ $row->name->{'name-EUen'} }} icon"></a>
+        <a href="{{ route('bug',['id'=>$i]) }}"><img src="{{ $row->icon_uri }}" alt="{{ $row->name->{'name-EUen'} }} icon"></a>
         <p>Price : {{ $row->price }} bells</p>
         <p>Rarity : {{ $row->availability->rarity }}</p>
       </div>
